f9de2af95ee9fb44dfeea644532f0172d4dc134f
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-05 14:50:43 +0700

    xong

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, StatusBar, StyleSheet, TouchableOpacity } from 'react-native';
+
+StatusBar.setHidden(true);
 
 export default class App extends Component {
     render() {
         return (
-            <View style={containerStyle}>
-                
+            <View style={styles.container}>
+               <Text style={styles.title}>Khoa Pham</Text> 
+               <TouchableOpacity style={styles.button}>
+                   <Text style={styles.buttonText}>Add</Text>
+               </TouchableOpacity>
             </View>
         );
     }
 }
 
-const containerStyle = {
-    flex: 1,
-    backgroundColor: 'lightblue'
-}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'lightblue',
+        paddingTop: 15,
+        alignItems: 'center'
+    },
+    title: {
+        color: 'blue',
+        fontSize: 25
+    },
+    button: {
+        padding: 20,
+        backgroundColor: '#6F5595',
+        width: 100,
+        borderRadius: 10,
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 25
+    }
+});
