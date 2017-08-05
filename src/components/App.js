9b5fa7a7e1ca99f6b7aa19b38d34f79933b5f288
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-05 14:38:54 +0700

    native

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class App extends Component {
+    render() {
+        return (
+            <View style={containerStyle}>
+                
+            </View>
+        );
+    }
+}
+
+const containerStyle = {
+    flex: 1,
+    backgroundColor: 'lightblue'
+}
